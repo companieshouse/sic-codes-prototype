b8376a199f98e1d3073c403c2719a4461b51fe54
Michael Gearon <50828707+mgearon-ch@users.noreply.github.com>
2021-11-09 10:34:57 +0000

    Changes

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Standard Industrial Classification (SIC) codes',
 
   // Default port that prototype runs on
   port: '3000',
